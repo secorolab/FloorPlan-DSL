b8d3cf855a55e5d4d7c3606b810dd09c218f7845
Samuel Parra <9706510+samuelpg@users.noreply.github.com>
2022-06-30 10:28:19 +0200

    WIP variation generation

--- src/exsce_floorplan/variation/exsce_variations.py
+++ src/exsce_floorplan/variation/exsce_variations.py
@@ -14,17 +14,22 @@ from textx import metamodel_for_language
 import numpy as np 
 import numpy.random as random
 
+def pose_json(pose):
+
+    return {
+        'translation' : {
+            'x' : pose.translation.x.value,
+            'y' : pose.translation.y.value,
+            'z' : pose.translation.z.value if not pose.translation.z is None else 0 
+        },
+        'rotation' : pose.rotation.value
+    }
+
 def space_json(space):
     return {
         'name' : space.name,
         'location' : {
-            'pose' : {
-                'translation' : {
-                    'x' : space.location.pose.translation.x.value,
-                    'y' : space.location.pose.translation.y.value,
-                },
-                'rotation' : space.location.pose.rotation.value
-            },
+            'pose' : pose_json(space.location.pose),
             'from' : {
                 'world' : space.location.from_frame.world,
                 'ref' : space.location.from_frame.ref,
@@ -41,6 +46,20 @@ def space_json(space):
         'wall_height' : space.wall_height
     }
 
+def wall_opening_json(wall_opening):
+    print(vars(wall_opening.wall_a))
+    return {
+        'entryway' : wall_opening.entryway,
+        'window'  : wall_opening.window,
+        'name' : wall_opening.name,
+        'in' : {
+            'wall_a' : wall_opening.wall_a,
+            'wall_b' : wall_opening.wall_b
+        },
+        'shape' : wall_opening.shape,
+        'pose' : pose_json(wall_opening.pose)
+    }
+
 def get_floorplan_as_json(flp_model):
     
     # convert the original model into a context dictionary
@@ -48,23 +67,32 @@ def get_floorplan_as_json(flp_model):
         'floorplan_name' : flp_model.name,
         'name' : flp_model.name,
         'spaces' : [space_json(space) for space in flp_model.spaces],
+        'wall_openings' : [wall_opening_json(wall_opening) for wall_opening in flp_model.wall_openings]
     }
-
+    print(context)
     return context
 
 def sample(context, variations):
 
     for var in variations.variations:
-        print(var.ref)
+
         for att in var.attributes:
             fqn = att.fqn.split('.')
-            print(fqn)
+            
             aux = {}
             if var.ref.__class__.__name__ == 'Space':
                 for i, space in enumerate(context["spaces"]):
                     if space["name"] == var.ref.name:
                         aux = context['spaces'][i]
                         break
+            elif var.ref.__class__.__name__ == 'WallOpening':
+                for i, wall_opening in enumerate(context["wall_openings"]):
+                    if wall_opening["name"] == var.ref.name:
+                        aux = context['wall_openings'][i]
+                        break
+            elif var.ref.__class__.__name__ == 'FloorFeature':
+                pass
+
             if len(fqn) == 1:
                 aux[fqn[0]] = att.distribution.sample()
             else:
@@ -75,8 +103,6 @@ def sample(context, variations):
                         name = fqn.pop(0)
                         aux[name] = att.distribution.sample()
                         break
-            
-        print(context)
 
     return context
 
@@ -92,7 +118,6 @@ def variation_floorplan_generator(metamodel, var_model, output_path, overwrite, 
     _path, filename = os.path.split(full_path)
     path = os.path.join(_path, 'templates')
     context = get_floorplan_as_json(flp_model)
-
     variations = custom_args["variations"]
     output = custom_args["output"]
 
