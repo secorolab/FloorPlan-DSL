9495893fa0ffa667de194826efa254c471ec4ab6
Samuel Parra <9706510+samuelpg@users.noreply.github.com>
2022-11-18 10:47:43 +0100

    Added unique name constraint check

--- src/exsce_floorplan/floor_plan/processors/processors.py
+++ src/exsce_floorplan/floor_plan/processors/processors.py
@@ -34,3 +34,34 @@ def feature_obj_processor(feature):
     #             space=feature.parent.name),
     #         **get_location(feature))
     pass
+
+def unique_names_processor(floorplan):
+
+    names = []
+
+    for space in floorplan.spaces:
+
+        if not space.name in names:
+            names.append(space.name)
+        else:
+            raise TextXSemanticError(
+                'Spaces and features must have unique names',
+                **get_location(space))          
+
+        for feature in space.floor_features:
+            fqn = "{space}.{feature}".format(space=space.name, feature=feature.name)
+            if not fqn in names:
+                names.append(fqn)
+            else:
+                raise TextXSemanticError(
+                    'Spaces and features must have unique names',
+                    **get_location(feature))
+        
+    for opening in floorplan.wall_openings:
+        if not opening.name in names:
+            names.append(opening.name)
+        else:
+            raise TextXSemanticError(
+                'Spaces and features must have unique names',
+                **get_location(opening))    
+
