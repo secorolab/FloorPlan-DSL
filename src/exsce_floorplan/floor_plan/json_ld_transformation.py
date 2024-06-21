10d9d11b8691a9526b465d36c5795800e59d5212
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2024-06-21 17:24:27 +0200

    Remove extra folder when generating JSON-LD

--- src/exsce_floorplan/floor_plan/json_ld_transformation.py
+++ src/exsce_floorplan/floor_plan/json_ld_transformation.py
@@ -18,30 +18,27 @@ def jsonld_floorplan_generator(
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
     skeleton = build_skeleton_graph(model, output_path)
-    shape = build_shape_graph(model, output_path)
-    spatial_relations = build_spatial_relations_graph(model, output_path)
-    floorplan = build_floorplan_graph(model, output_path)
-    coordinate = build_floorplan_coordinate_graph(model, output_path)
-
-    directory = os.path.join(output_path, "{name}_json_ld".format(name=model.name))
-    print(directory)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    with open("{}/skeleton.json".format(directory), "w") as file:
+    with open("{}/skeleton.json".format(output_path), "w") as file:
         json.dump(skeleton, file, indent=4)
 
-    with open("{}/shape.json".format(directory), "w") as file:
+    shape = build_shape_graph(model, output_path)
+    with open("{}/shape.json".format(output_path), "w") as file:
         json.dump(shape, file, indent=4)
 
-    with open("{}/spatial_relations.json".format(directory), "w") as file:
+    spatial_relations = build_spatial_relations_graph(model, output_path)
+    with open("{}/spatial_relations.json".format(output_path), "w") as file:
         json.dump(spatial_relations, file, indent=4)
 
-    with open("{}/floorplan.json".format(directory), "w") as file:
+    floorplan = build_floorplan_graph(model, output_path)
+    with open("{}/floorplan.json".format(output_path), "w") as file:
         json.dump(floorplan, file, indent=4)
 
-    with open("{}/coordinate.json".format(directory), "w") as file:
+    coordinate = build_floorplan_coordinate_graph(model, output_path)
+    with open("{}/coordinate.json".format(output_path), "w") as file:
         json.dump(coordinate, file, indent=4)
 
 
