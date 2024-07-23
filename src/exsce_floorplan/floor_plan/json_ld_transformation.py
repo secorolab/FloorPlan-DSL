9f0686db56b03783e7b75df3ad885e88899493d5
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2024-07-23 08:54:55 +0200

    Generate all floorplan templates

--- src/exsce_floorplan/floor_plan/json_ld_transformation.py
+++ src/exsce_floorplan/floor_plan/json_ld_transformation.py
@@ -20,13 +20,21 @@ def jsonld_floorplan_generator(
     context["model"] = model
 
     this_folder = os.path.dirname(__file__)
-    print(this_folder)
-    # template_folder = os.path.join(this_folder, '../templates/skeleton.json.jinja')
-    # template_folder = os.path.join(this_folder, '../templates/shape.json.jinja')
-    # template_folder = os.path.join(this_folder, '../templates/spatial_relations.json.jinja')
-    # template_folder = os.path.join(this_folder, '../templates/floorplan.json.jinja')
-    template_folder = os.path.join(this_folder, "../templates/coordinate.json.jinja")
-    print("Template folder: ", template_folder)
+    # If given a directory instead of a single template, textX-Jinja copies the entire file/folder tree to the output folder
+    # For now we invoke the generator for each template we are interested in
+    # TODO Find more optimal way to handle templates
+    template_folder = os.path.join(this_folder, "../templates/skeleton.json.jinja")
 
     # Run Jinja generator
+    textx_jinja_generator(template_folder, output_path, context, overwrite=True)
+
+    template_folder = os.path.join(this_folder, "../templates/shape.json.jinja")
+    textx_jinja_generator(template_folder, output_path, context, overwrite=True)
+    template_folder = os.path.join(
+        this_folder, "../templates/spatial_relations.json.jinja"
+    )
+    textx_jinja_generator(template_folder, output_path, context, overwrite=True)
+    template_folder = os.path.join(this_folder, "../templates/floorplan.json.jinja")
+    textx_jinja_generator(template_folder, output_path, context, overwrite=True)
+    template_folder = os.path.join(this_folder, "../templates/coordinate.json.jinja")
     textx_jinja_generator(template_folder, output_path, context, overwrite=True)
