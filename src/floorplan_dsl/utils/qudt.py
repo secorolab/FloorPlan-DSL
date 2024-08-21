a40bd392061ac2555f1b79eeab1ddc1fd8ffa4c6
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2024-08-21 23:18:59 +0200

    Remove print statement

--- src/floorplan_dsl/utils/qudt.py
+++ src/floorplan_dsl/utils/qudt.py
@@ -7,7 +7,6 @@ def convert_angle_units(model, unit):
         if unit == "deg" and model.unit == "rad":
             model.unit = "deg"
             model.value = np.rad2deg(model.value)
-            print(model.value)
         elif unit == "rad" and model.unit == "deg":
             model.unit = "rad"
             model.value = np.deg2rad(model.value)
